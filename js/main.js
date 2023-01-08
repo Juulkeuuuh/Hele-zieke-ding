f8f3467f6b0ae3595fb9f57daa178fe25b2b055f
Juulkeuuuh <90319980+Juulkeuuuh@users.noreply.github.com>
2023-01-08 14:38:38 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@
 //storage shit
 var numKeys = localStorage.length;
 const keyNaam = localStorage.getItem('naam');
-$('#nameText').html('Hi '+keyNaam+'!');
 const keyColour = localStorage.getItem('colour');
 if (keyColour) {
     $('#colourText').html(keyColour+'?! I like that colour too!');
@@ -16,10 +15,6 @@ if (numKeys > 1) {
     $('html, body').animate({ scrollTop: $('#scene-'+0).offset().top }, 'slow');
 }
 
-
-
-console.log(numKeys);
-
 function start(){
     $('html, body').animate({ scrollTop: $('#scene-1').offset().top }, 'slow');
     $(".logo").css('width', '30vw');
@@ -28,14 +23,21 @@ function start(){
 function nome() {
     const naam = $( "#name" ).val();
     localStorage.setItem('naam', naam);
-    $('#nameText').html('Hi '+naam+'!');
+    $('h5').html(naam);
     $('html, body').animate({ scrollTop: $('#scene-2').offset().top }, 'slow');
+    $('.guy').css('display', 'block');
 }
  function kleurtjes() {
-    let colour =  $("#colour").val();
-    localStorage.setItem('colour', colour);
-    $("body").css('background', colour);
-    $('#colourText').html(colour+'?! I like that colour too!');
+    let colour1 =  $("#colour1").val();
+    localStorage.setItem('colour1', colour1);
+    $(".cls-1").css('fill', colour1);
+    let colour2 =  $("#colour2").val();
+    localStorage.setItem('colour2', colour2);
+    $(".cls-2").css('fill', colour2);
+    let colour3 =  $("#colour3").val();
+    localStorage.setItem('colour3', colour3);
+    $(".cls-3").css('fill', colour3);
+    // $('#colourText').html(colour+'?! I like that colour too!');
     $('html, body').animate({ scrollTop: $('#scene-3').offset().top }, 'slow');
  }
 
@@ -49,6 +51,7 @@ function clearStorage() {
     localStorage.clear();
     $("body").css('background', '#F7F7FF');
     $('html, body').animate({ scrollTop: $('#scene-0').offset().top }, 'slow');
+    $('.guy').css('display', 'none');
 }
 
 
